f0f036939fa2f5acb5ba4a1bf8de3addd661b6a2
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-03-24 04:10:26 +0300

    update1

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisID { get; set; }
+        // ÜRÜN
+        // CARİ
+        // PERSONEL
+
+        public DateTime Tarih { get; set; }
+
+        public int Adet { get; set; }
+
+        public decimal Fiyat { get; set; }
+
+        public decimal ToplamTutar { get; set; }
+    }
+}
