3464ae54ac8f61eeadfea33df49741d52335b613
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-04-13 05:25:49 +0300

    Login/session/CariPanel

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,32 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        Context c = new Context();
+        // GET: CariPanel
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+        public ActionResult Siparislerim()
+        {
+            //sisteme giriş yapan mail adresini sesion olarak atadık ondan sonra  daha sonra id değişkeini tanımladık bu mail adresinin id'sini aldım sonra değerler adında değiken oluşturdum satişharekette carid ile benim aldım id değikenin listesini geriye döndürecek 
+            var mail = (string)Session["CariMail"];
+            var id =c.Carilers.Where(x=>x.CariMail==mail.ToString()).Select(y=>y.CariID).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            return View(degerler);  
+        }
+
+    }
+}
--- MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,20 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class YapilacakController : Controller
     {
         // GET: Yapilacak
+        Context c =new Context();
         public ActionResult Index()
         {
-            return View();
+            var deger1=c.Carilers.Count().ToString();
+            ViewBag.d1=deger1;
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Kategoris.Count().ToString();
+            ViewBag.d3 = deger3;
+            var deger4 = (from x in c.Carilers select x.CariSehir).Distinct().Count().ToString();
+            ViewBag.d4 = deger4;
+            var yapilacaklar = c.Yapilacaks.ToList();
+
+            return View(yapilacaklar);
         }
     }
 }
