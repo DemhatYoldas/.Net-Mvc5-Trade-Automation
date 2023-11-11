6818bfede65250213dbd92dc39b0ab426615a481
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-11-11 03:24:42 +0300

    File Upload

--- MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Antlr.Runtime.Tree;
 using MvcOnlineTicariOtomasyon.Models.Siniflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
@@ -34,6 +36,17 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpPost]
         public ActionResult PersonelEkle(Personel d)
         {
+            if (Request.Files.Count>0)
+            {
+                string dosyaadi = Path.GetFileName(Request.Files[0].FileName);
+                string uzanti = Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaadi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                d.PersonelGorsel="/Image/"+dosyaadi+uzanti;
+                
+            }
+            d.Durum=true;
+
             c.Personels.Add(d);
             c.SaveChanges();
             return RedirectToAction("Index");
@@ -63,6 +76,15 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 
         public ActionResult PersonelGuncelle(Personel d)
         {
+            if (Request.Files.Count > 0)
+            {
+                string dosyaadi = Path.GetFileName(Request.Files[0].FileName);
+                string uzanti = Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaadi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                d.PersonelGorsel = "/Image/" + dosyaadi + uzanti;
+
+            }
             var per = c.Personels.Find(d.PersonelID);
             per.PersonelAd = d.PersonelAd;
             per.PersonelSoyad = d.PersonelSoyad;
